2f5daaa4c1aa4f93d6b3a251c4820fc7bea56acb
hitoshi555 <hitoshi1845@gmail.com>
2022-01-01 19:37:44 +0900

    回答をみて解いた

--- competitiveProfessional90Questions/crossSum.js
+++ competitiveProfessional90Questions/crossSum.js
@@ -11,20 +11,44 @@ b.列が１の総和を出す
 
 行列パターン2（〜n）
 https://qiita.com/cotton11aq/items/a8b93d61fca8f83ffaae#%E8%A1%8C%E5%88%97%E3%83%91%E3%82%BF%E3%83%BC%E3%83%B32n
+
+自力で解けたか？
+x
+以下のユーザーが提出したコードを見た。
+https://atcoder.jp/contests/typical90/submissions/27422747
 */
 
-function main(input) {
-  const args = input.split("\n").map((n) => n.split(" "));
-  const n = args[0][0];
-  const x = args[0][1];
-  const v = args.slice(1, n + 1).map((n) => parseInt(n[0], 10));
-  const p = args.slice(1, n + 1).map((n) => parseInt(n[1], 10));
 
-  console.log(args); // [ [ '2', '10' ], [ '200', '5' ], [ '350', '3' ] ]
-  console.log(n); // 2
-  console.log(x); // 15
-  console.log(v); // [ 200, 350 ]
-  console.log(p); // [ 5, 3 ]
+'use strict';
+//strict modeとは何か?
+//https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Strict_mode
+
+function main(input) {
+  const args = input.trim().split('\n');
+  //trim 空白を消してくらえる
+  const [H, W] = args[0].split(' ').map(Number);
+  const A = [];
+
+  //----------------------点線部分のイメージがつかない
+  for (let i = 0; i < H; i++)A[i] = args[i + 1].split(' ').map(Number);
+
+  let sumRow = new Array(H).fill(0), sumCol = new Array(W).fill(0);
+
+  for (let i = 0; i < H; i++) {
+    for (let j = 0; j < W; j++) {
+      sumRow[i] += A[i][j];
+      sumCol[j] += A[i][j];
+    }
+  }
+  //------------------------
+
+  for (let i = 0; i < H; i++) {
+    for (let j = 0; j < W; j++) {
+      A[i][j] = sumRow[i] + sumCol[j] - A[i][j];
+    }
+  }
+
+  return A.map(v => v.join(' ')).join('\n');
 }
 
-main(require("fs").readFileSync("/dev/stdin", "utf8"));
+console.log(main(require('fs').readFileSync('/dev/stdin', 'utf8')));
