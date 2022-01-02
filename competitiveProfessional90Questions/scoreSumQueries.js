2ea904932cfe4e7b69774aa1745bd30581967101
hitoshi555 <hitoshi1845@gmail.com>
2022-01-02 19:05:13 +0900

    新しい問題を追加

---
+++ competitiveProfessional90Questions/scoreSumQueries.js
@@ -0,0 +1,18 @@
+/*
+問題
+https://atcoder.jp/contests/typical90/tasks/typical90_j
+
+回答手順
+
+自力で解けたか？
+
+以下のユーザーが提出したコードを見た。
+
+*/
+
+'use strict';
+'use strict';
+
+function main(input) { }
+
+console.log(main(require('fs').readFileSync('/dev/stdin', 'utf8')));
