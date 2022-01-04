7233fb077f50452cc3b21f9a5385a76e122e42c3
hitoshi555 <hitoshi1845@gmail.com>
2022-01-04 22:41:05 +0900

    少し解いた

--- competitiveProfessional90Questions/scoreSumQueries.js
+++ competitiveProfessional90Questions/scoreSumQueries.js
@@ -27,7 +27,23 @@ x
 'use strict';
 
 function main(input) {
+  const args = input.trim().split('\n');
+  const N = Number(args[0]);
+  const student = [];
+  for (let i = 0; i < N; i++)student[i] = args[i + 1].split(' ').map(Number);
+  const question = [];
+  let j = 0;
+  for (let i = N + 2; i < args.length; i++) {
+    question[j] = args[i].split(' ').map(Number);
+    j += 1;
+  }
+  console.log(student)
+  console.log(question)
 
+  // 1組の和を順番に足した配列を作成する(累積和)
+  // 2組の和を順番に足した配列を作成する(累積和)
+  const A = student.map((v) => { })
+  return N
 }
 
 console.log(main(require('fs').readFileSync('/dev/stdin', 'utf8')));
