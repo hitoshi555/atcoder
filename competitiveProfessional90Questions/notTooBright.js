be7e4e6672450ce999a935cf77f845463b10747a
Hitoshi Takahashi <40290379+hitoshi555@users.noreply.github.com>
2022-01-12 19:59:36 +0900

    mac
    他の人の回答を見て解いてみた。
    得問題の追加

--- competitiveProfessional90Questions/notTooBright.js
+++ competitiveProfessional90Questions/notTooBright.js
@@ -9,11 +9,24 @@ https://atcoder.jp/contests/typical90/tasks/typical90_ag
 自力で解けたか？
 x
 以下のユーザーが提出したコードを見た。
-
+https://atcoder.jp/contests/typical90/submissions/27071756
+https://atcoder.jp/contests/typical90/submissions/23282435
 */
 
-'use strict';
+"use strict";
 
-function main(input) { }
+function main(input) {
+  const tmp = input.trim().split("\n");
+  const [H, W] = tmp[0].split(" ").map(Number);
+
+  if (H === 1 || W === 1) {
+    return console.log(H * W);
+  } else {
+    const ans = Math.round(H / 2) * Math.round(W / 2);
+    //Math.roud() https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Math/round
+    //奇数の場合を考えてMath.round()で近似値を出している
+    return console.log(ans);
+  }
+}
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
---
+++ competitiveProfessional90Questions/select5.js
@@ -0,0 +1,19 @@
+/*
+問題
+https://atcoder.jp/contests/typical90/tasks/typical90_bc
+
+回答手順
+
+解説見た後
+
+自力で解けたか？
+
+以下のユーザーが提出したコードを見た。
+
+*/
+
+"use strict";
+
+function main(input) {}
+
+main(require("fs").readFileSync("/dev/stdin", "utf8"));
