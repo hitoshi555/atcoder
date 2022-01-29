f0be60688255d7605c57adfa321a4f6128771fb7
hitoshi555 <hitoshi1845@gmail.com>
2022-01-29 15:30:38 +0900

    他の人のコードを見た

--- AtCoderBeginnersSelection/ABC083B-SomeSums.js
+++ AtCoderBeginnersSelection/ABC083B-SomeSums.js
@@ -20,29 +20,31 @@ x
 https://qiita.com/drken/items/fd4e5e3630d0f5859067#5-%E9%81%8E%E5%8E%BB%E5%95%8F%E7%B2%BE%E9%81%B8-10-%E5%95%8F
 
 自分が書いたコード
-コード長 : Byte
-メモリ : KB
-実行時間 : ms
+コード長 : 591Byte
+メモリ : 33508KB
+実行時間 : 64ms
 
 メモリが小さい
-
-コード長 : Byte
-メモリ : KG
-実行時間 : ms
+https://atcoder.jp/contests/abs/submissions/2264509
+コード長 : 804Byte
+メモリ : 7372G
+実行時間 : 53ms
 
 実行時間が一番短い
-
-コード長 : Byte
-メモリ : KB
-実行時間 : ms
+https://atcoder.jp/contests/abs/submissions/12523419
+コード長 : 490Byte
+メモリ : 31716KB
+実行時間 : 49ms
 
 コード長が一番短い
-
-コード長 : 89Byte
-メモリ : 7372KB
-実行時間 : 52ms
+https://atcoder.jp/contests/abs/submissions/11223836
+コード長 : 239Byte
+メモリ : 10984KB
+実行時間 : 79ms
 
 parseIntしないと小数点が発生しうまくACにならなかった
+実行時間が一番短い人のコードはMath.floor()を使っていた
+Math.floor() https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
 */
 
 'use strict';
