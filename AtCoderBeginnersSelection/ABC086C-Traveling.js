e0857c0de0765c4d9d0f6f8a2722cb61a3e01dc3
hitoshi555 <hitoshi1845@gmail.com>
2022-02-05 17:43:51 +0900

    次に解く問題を追加した

---
+++ AtCoderBeginnersSelection/ABC086C-Traveling.js
@@ -0,0 +1,43 @@
+/*
+問題
+https://atcoder.jp/contests/abs/tasks/arc089_a
+
+回答手順
+
+解説見た後
+
+自力で解けたか？
+
+以下のユーザーが提出したコードを見た。
+
+自分が書いたコード
+コード長 : Byte
+メモリ : KB
+実行時間 : ms
+
+1.メモリが小さい
+
+コード長 : Byte
+メモリ : KG
+実行時間 : ms
+
+2.実行時間が一番短い
+
+コード長 : Byte
+メモリ : KB
+実行時間 : ms
+
+3.コード長が一番短い
+
+コード長 : Byte
+メモリ : KB
+実行時間 : ms
+
+
+*/
+
+"use strict";
+
+function main(input) {}
+
+main(require("fs").readFileSync("/dev/stdin", "utf8"));
