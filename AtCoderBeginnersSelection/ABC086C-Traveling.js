11951cead837dc80e743148dfd1e50b44979e64a
hitoshi555 <hitoshi1845@gmail.com>
2022-02-09 22:10:21 +0900

    実行速度の早い人のコードを見た

--- AtCoderBeginnersSelection/ABC086C-Traveling.js
+++ AtCoderBeginnersSelection/ABC086C-Traveling.js
@@ -11,6 +11,8 @@ x
 
 以下のユーザーが提出したコードを見た。
 https://qiita.com/drken/items/fd4e5e3630d0f5859067#%E7%AC%AC-10-%E5%95%8F--abc-086-c---traveling-300-%E7%82%B9
+実行時間が一番速い人を見た
+https://atcoder.jp/contests/abs/submissions/2212945
 
 自分が書いたコード
 コード長 : Byte
@@ -24,10 +26,10 @@ https://qiita.com/drken/items/fd4e5e3630d0f5859067#%E7%AC%AC-10-%E5%95%8F--abc-0
 実行時間 : ms
 
 2.実行時間が一番短い
-
-コード長 : Byte
-メモリ : KB
-実行時間 : ms
+https://atcoder.jp/contests/abs/submissions/2212945
+コード長 : 1392Byte
+メモリ : 24840KB
+実行時間 : 97ms
 
 3.コード長が一番短い
 
@@ -39,8 +41,56 @@ https://qiita.com/drken/items/fd4e5e3630d0f5859067#%E7%AC%AC-10-%E5%95%8F--abc-0
 ・パリティ 「偶数」と「奇数」に関する性質
 */
 
-"use strict";
+function Main(inputData) {
+  init(inputData);
 
-function main(input) { }
+  var n = inI(), nt = 0, nx = 0, ny = 0;
 
-main(require("fs").readFileSync("/dev/stdin", "utf8"));
+  for (i = 0; i < n; i++) {
+    var t = inI(), x = inI(), y = inI();
+
+    var r = Math.abs(nx - x) + Math.abs(ny - y);
+
+    if (t - nt < r || ((t - nt) - r) & 1) {
+      out("No");
+      return 0;
+    }
+    nt = t;
+    nx = x;
+    ny = y;
+  }
+  out("Yes");
+  //init(), inI(), out()など事前に用意している。
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////
+var input, inputCnt;
+
+// "実行する"ボタンを押した時に実行される関数 (デバッグ用)
+function debug() {
+  Main(document.getElementById("input").value);
+}
+
+function init(inputData) {
+  //配列inputに順番に代入
+  input = inputData.replace(/\n/g, " ");
+  input = input.split(" ");
+  inputCnt = 0;
+}
+//output
+const out = (output) => { console.log(output) }
+const outln = (output) => { console.log(output + "\n") }
+
+//input
+const inS = () => { return input[inputCnt++] };
+const inI = () => { return parseInt(inS(), 10) };
+const inF = () => { return parseFloat(inS()) };
+
+//prototype
+Array.prototype.rsort = function () {
+  return this.sort(function (a, b) { return (a < b ? 1 : -1); });
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+Main(require("fs").readFileSync("/dev/stdin", "utf8"));
